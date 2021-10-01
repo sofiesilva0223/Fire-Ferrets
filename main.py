db1189c9b9f9a74add0287449bd4c9b12a6ce405
sofiesilva0223 <89554609+sofiesilva0223@users.noreply.github.com>
2021-10-01 12:28:36 -0700

    Add files via upload
    Main .python file to turn in

---
+++ main.py
@@ -0,0 +1,19 @@
+import csv
+import json
+import xml
+#HW2
+#Fire Ferrets: Cassidy Chu, Sofia Silva, Edin Schneider, Matt Borja
+#CMPE 131, section 01
+
+#Reads filename as an argument
+def readTxt (filename):
+    with open(filename) as fileObject:
+        contents = fileObject.read()
+    print(contents)  #will print contents of NFL.txt
+readTxt('NFL.txt')
+
+#Prompt user to choose c,j,x code:
+
+#Function for coverting file to c, j and x:
+
+
